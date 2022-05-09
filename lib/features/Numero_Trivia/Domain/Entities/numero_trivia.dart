4c71e05f898b9ab7298dffa3026e7f175d7d0982
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-05-08 17:49:10 -0700

    Started creating entities

---
+++ lib/features/Numero_Trivia/Domain/Entities/numero_trivia.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+
+class NumeroTrivia extends Equatable{
+  final String text;
+  final int number;
+
+  NumeroTrivia({required this.text,required this.number});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+
+  
+
+}
